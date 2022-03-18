51a81c6ca8f8cb61c06a58dabab24943f5ce340d
Megan Krenbrink <megan.krenbrink@gmail.com>
2022-03-18 10:55:30 -0700

    initial files
    current working files

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+// FLYOUT NAVIGATION BAR //
+// Resize the width of panel to show menu
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "230px";
+}// Resize the width to hide menu
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+// CHANGE PICTURE IN FOOTER //
+// Change the image of cartoon when user clicks
+function changeMeg() {
+  document.getElementById('aniMeg').src = "images/animeg_heart.png";
+}
+
+// ISOTOPE FILTERING //
+// initialize Isotope, set class and sizing option
+var $grid = $('.grid').isotope({
+  itemSelector: '.work-card',
+  layoutMode: 'fitRows',
+});
+
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  $grid.isotope({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
+// ABOUT ME READ MORE TOGGLE //
+$( "#myBtn" ).click(function() {
+  $( "#more" ).slideToggle( "slow" );
+  $(this).text(function(i, v){
+    return v === 'Read Less' ? 'Read More' : 'Read Less'
+ })
+});
+
